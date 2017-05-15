d8421dd43beb46a34535a3d7287ea7b8274e88a7
Fatima Hammami <en.f.h@live.com>
2017-05-15 12:55:18 +0300

    ss

--- messenger.js
+++ messenger.js
@@ -108,7 +108,7 @@ const actions = {
     const {sessionId, context, entities} = request;
     const {text, quickreplies} = response;
     if(request.entities){
-      console.log("user said...",request.entities);
+      console.log("user said...",request.text,request.entities);
     }else{
       console.log("user said2...,,,",request)
     }
